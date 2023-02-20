b8b4fb4c677f8e3b16c25300ca46cfceecd42ad1
Kamron or Камрон <kamrush6429@gmail.com>
2023-02-20 11:53:03 +0500

    update project

---
+++ components/TheNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="nav">
+    <h3>Nuxt App</h3>
+  </nav>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 60px;
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
-    <navbar />
-    <main>
-      <div class="container">
-        <h1>nuxt</h1>
-      </div>
-    </main>
+    <TheNavogation />
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
-export default {
-  components: {Navbar},
-  name: 'IndexPage'
-}
+export default {}
 </script>
+
+
+<style>
+</style>
