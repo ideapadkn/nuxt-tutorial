4fee56d570754f1e71421998a76c9acc68188863
Kamron or Камрон <kamrush6429@gmail.com>
2023-02-20 11:02:48 +0500

    added bootstrap and navbar, but i dont working with about

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <Tutorial/>
+  <div>
+    <navbar />
+    <main>
+      <div class="container">
+        <h1>nuxt</h1>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
+  components: {Navbar},
   name: 'IndexPage'
 }
 </script>
